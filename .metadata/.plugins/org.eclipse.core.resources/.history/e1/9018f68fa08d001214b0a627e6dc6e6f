df38b2081303aa8cba3e69f1ac259440aa127938
Tom Atnip <tom.atnip@gmail.com>
2013-03-15 15:12:23 -0400

    Allow CellBlock to recieve guesses

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/e1/9018f68fa08d001214b0a627e6dc6e6f
@@ -0,0 +1,6 @@
+package SudokuTest;
+
+
+public class CellBlockTest {
+
+}
--- Sudoku/src/Sudoku/CellBlock.java
+++ Sudoku/src/Sudoku/CellBlock.java
@@ -1,5 +1,26 @@
 package Sudoku;
 
+import java.util.ArrayList;
+
 public class CellBlock {
+	private ArrayList<Integer> guess;
+	
+	public CellBlock(){
+		guess = new ArrayList<Integer>();
+	}
+
+	public void guess(int... ints) {
+		for (int i : ints) {
+			if(guess.contains(i)){
+				throw new IllegalArgumentException("Guesses must contain all unique values...");
+			}
+			guess.add(i);
+		}
+
+	}
+
+	public ArrayList<Integer> value() {
+		return guess;
+	}
 
 }
