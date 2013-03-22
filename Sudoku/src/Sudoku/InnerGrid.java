93a24f23ccf36a80e57dd053df7244681f8613e3
George Mammarella <mammargs@rose-hulman.edu>
2013-03-22 00:20:03 -0400

    Adds code to pass initialize test for InnerGrid

--- Sudoku/src/Sudoku/InnerGrid.java
+++ Sudoku/src/Sudoku/InnerGrid.java
@@ -1,5 +1,18 @@
 package Sudoku;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class InnerGrid {
+	
+ArrayList<CellBlock> gridList;
+	
+	public InnerGrid(ArrayList<CellBlock> gridList){
+		this.gridList = new ArrayList<CellBlock>();
+		Iterator<CellBlock> iterator = gridList.iterator();
+		while(iterator.hasNext()) {
+			this.gridList.add(iterator.next());
+		}
+	}
 
 }
