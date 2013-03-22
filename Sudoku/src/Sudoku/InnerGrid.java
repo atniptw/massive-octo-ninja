34ae011df3b62ecac928075091b512f4e66a7e4b
George Mammarella <mammargs@rose-hulman.edu>
2013-03-22 00:24:38 -0400

    Adds Accept Filled Grid test InnerGrid

--- Sudoku/src/Sudoku/InnerGrid.java
+++ Sudoku/src/Sudoku/InnerGrid.java
@@ -1,17 +1,36 @@
 package Sudoku;
 
+import static org.junit.Assert.assertEquals;
+
 import java.util.ArrayList;
 import java.util.Iterator;
 
+import org.junit.Test;
+
 public class InnerGrid {
-	
-ArrayList<CellBlock> gridList;
-	
-	public InnerGrid(ArrayList<CellBlock> gridList){
+
+	ArrayList<CellBlock> gridList;
+
+	public InnerGrid(ArrayList<CellBlock> gridList) {
 		this.gridList = new ArrayList<CellBlock>();
 		Iterator<CellBlock> iterator = gridList.iterator();
-		while(iterator.hasNext()) {
+		while (iterator.hasNext()) {
 			this.gridList.add(iterator.next());
+		}
+	}
+
+	@Test
+	public void testInnerGridAcceptsFilledGrid() {
+		ArrayList<CellBlock> gridCellList = new ArrayList<CellBlock>();
+		for (int i = 0; i < 9; i++) {
+			CellBlock cell = new CellBlock();
+			cell.setAnswer(i + 1);
+			gridCellList.add(cell);
+		}
+		InnerGrid grid = new InnerGrid(gridCellList);
+		ArrayList<CellBlock> gridList = grid.getGridList();
+		for (int i = 0; i < 9; i++) {
+			assertEquals(i + 1, gridList.get(i).getAnswer());
 		}
 	}
 
