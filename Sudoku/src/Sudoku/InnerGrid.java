77e8add4f6a9347ca66f69c8047fb589b1426141
George Mammarella <mammargs@rose-hulman.edu>
2013-03-22 00:28:28 -0400

    Adds code to pass Accept Filled Grid test InnerGrid

--- Sudoku/src/Sudoku/InnerGrid.java
+++ Sudoku/src/Sudoku/InnerGrid.java
@@ -19,19 +19,9 @@ public class InnerGrid {
 		}
 	}
 
-	@Test
-	public void testInnerGridAcceptsFilledGrid() {
-		ArrayList<CellBlock> gridCellList = new ArrayList<CellBlock>();
-		for (int i = 0; i < 9; i++) {
-			CellBlock cell = new CellBlock();
-			cell.setAnswer(i + 1);
-			gridCellList.add(cell);
-		}
-		InnerGrid grid = new InnerGrid(gridCellList);
-		ArrayList<CellBlock> gridList = grid.getGridList();
-		for (int i = 0; i < 9; i++) {
-			assertEquals(i + 1, gridList.get(i).getAnswer());
-		}
+	public ArrayList<CellBlock> getGridList() {
+		return this.gridList;
+
 	}
 
 }
