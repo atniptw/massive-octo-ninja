cc41f1b8fc1b25d2fc9e142dc2f858c6567b2458
George Mammarella <mammargs@rose-hulman.edu>
2013-03-22 00:16:56 -0400

    Adds bare test class for InnerGrid

---
+++ Sudoku/src/SudokuTest/InnerGridTest.java
@@ -0,0 +1,14 @@
+package SudokuTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InnerGridTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
