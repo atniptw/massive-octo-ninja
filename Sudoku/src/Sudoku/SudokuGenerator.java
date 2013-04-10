7f2cf11d33f2d5a824869e4a7c6103285006ddd4
Tom Atnip <tom.atnip@gmail.com>
2013-04-10 17:30:12 -0400

    Generates Valid Rows

--- Sudoku/src/Sudoku/SudokuGenerator.java
+++ Sudoku/src/Sudoku/SudokuGenerator.java
@@ -11,28 +11,29 @@ public class SudokuGenerator {
 		int[][] rowRegions = new int[size][size];
 		for (int i = 0; i < size; i++) {
 			for (int j = 0; j < size; j++) {
-				addNumber(rowRegions, i, j);
+				addNumber(rowRegions, i, j, size);
 			}
 		}
 
 		return rowRegions;
 	}
 
-	public static void addNumber(int[][] rowRegions, int i, int j) {
+	public static void addNumber(int[][] rowRegions, int row, int column,
+			int size) {
+
 		Random generator = new Random();
 		ArrayList<Integer> val = new ArrayList<Integer>();
-		val.add(1);
-		val.add(2);
-		val.add(3);
-		val.add(4);
+		for (int i = 0; i < size; i++) {
+			val.add(i + 1);
+		}
+
 		int rnd = generator.nextInt(val.size());
-		rowRegions[i][j] = val.get(rnd);
-		
-		while (!isValid(rowRegions, i, j)) {
+		rowRegions[row][column] = val.get(rnd);
+
+		while (!isValid(rowRegions, row + 1, column + 1)) {
 			val.remove(rnd);
-			
 			rnd = generator.nextInt(val.size());
-			rowRegions[i][j] = val.get(rnd);
+			rowRegions[row][column] = val.get(rnd);
 		}
 
 	}
@@ -40,10 +41,11 @@ public class SudokuGenerator {
 	public static boolean isValid(int[][] rowRegions, int row, int column) {
 		ArrayList<Integer> testRegion = new ArrayList<Integer>();
 		for (int i = 0; i < row; i++) {
+			testRegion.clear();
 			for (int j = 0; j < column; j++) {
 				testRegion.add(rowRegions[i][j]);
 			}
-			
+
 			Collections.sort(testRegion);
 			for (int k = 0; k < testRegion.size() - 1; k++) {
 				if (testRegion.get(k).equals(testRegion.get(k + 1))) {
--- Sudoku/src/SudokuTest/SudokuGeneratorTest.java
+++ Sudoku/src/SudokuTest/SudokuGeneratorTest.java
@@ -17,14 +17,17 @@ public class SudokuGeneratorTest {
 
 	private static int[][] gameBoard;
 
-	/*
-	 * @BeforeClass public static void generateGameBoard() { gameBoard =
-	 * SudokuGenerator.generateBoard(4); for (int i = 0; i < 4; i++) { for (int
-	 * j = 0; j < 4; j++) { System.out.print(gameBoard[i][j]); }
-	 * System.out.println(); } }
-	 */
-	
-	
+	@BeforeClass
+	public static void generateGameBoard() {
+		gameBoard = SudokuGenerator.generateBoard(9);
+		for (int i = 0; i < 9; i++) {
+			for (int j = 0; j < 9; j++) {
+				System.out.print(gameBoard[i][j]);
+			}
+			System.out.println();
+		}
+	}
+
 	@Test
 	public void test81NumbersGenerated() {
 		assertEquals(9, gameBoard.length);
@@ -33,8 +36,8 @@ public class SudokuGeneratorTest {
 	@Test
 	public void testEachRowIsValid() {
 		SudokuStandardRegion region;
-		ArrayList<CellBlock> row = null;
 		for (int i = 0; i < 9; i++) {
+			ArrayList<CellBlock> row = new ArrayList<CellBlock>();
 			for (int j = 0; j < 9; j++) {
 				row.add(new CellBlock(gameBoard[i][j]));
 			}
@@ -46,8 +49,8 @@ public class SudokuGeneratorTest {
 	@Test
 	public void testEachColumnIsValid() {
 		SudokuStandardRegion region;
-		ArrayList<CellBlock> column = null;
 		for (int i = 0; i < 9; i++) {
+			ArrayList<CellBlock> column = new ArrayList<CellBlock>();
 			for (int j = 0; j < 9; j++) {
 				column.add(new CellBlock(gameBoard[j][i]));
 			}
@@ -62,17 +65,59 @@ public class SudokuGeneratorTest {
 				{ 4, 1, 2, 3 } };
 		assertFalse(SudokuGenerator.isValid(testBoard, 4, 4));
 	}
-	
+
 	@Test
 	public void testFindsInvalidRowSecondRow() {
 		int[][] testBoard = { { 1, 2, 3, 4 }, { 3, 3, 4, 1 }, { 3, 4, 1, 2 },
 				{ 4, 1, 2, 3 } };
 		assertFalse(SudokuGenerator.isValid(testBoard, 4, 4));
 	}
-	
+
 	@Test
-	public void testFindsInvalidNewNumberSecondRow(){
+	public void testFindsInvalidNewNumberSecondRow() {
 		int[][] testBoard = { { 1, 2, 3, 4 }, { 3, 3 } };
 		assertFalse(SudokuGenerator.isValid(testBoard, 2, 2));
+	}
+
+	@Test
+	public void testAddNextNumberToRowWithOneNumber() {
+		int[][] testBoard = { { 1, 0 } };
+		SudokuGenerator.addNumber(testBoard, 0, 1, 2);
+		assertEquals(2, testBoard[0][1]);
+	}
+
+	@Test
+	public void testAddNextNumberToRowWithTwoNumbers() {
+		int[][] testBoard = { { 1, 3, 0 } };
+		SudokuGenerator.addNumber(testBoard, 0, 2, 3);
+		assertEquals(2, testBoard[0][2]);
+	}
+
+	@Test
+	public void testAddNextNumberToRowWithThreeNumbers() {
+		int[][] testBoard = { { 1, 3, 4, 0 } };
+		SudokuGenerator.addNumber(testBoard, 0, 3, 4);
+		assertEquals(2, testBoard[0][3]);
+	}
+
+	@Test
+	public void testAddNextNumberToRowWithFourNumbers() {
+		int[][] testBoard = { { 1, 4, 2, 0 } };
+		SudokuGenerator.addNumber(testBoard, 0, 3, 4);
+		assertEquals(3, testBoard[0][3]);
+	}
+
+	@Test
+	public void testAddNextNumberToSecondRowWithThreeNumbers() {
+		int[][] testBoard = { { 1, 2, 3, 4 }, { 2, 3, 0 } };
+		SudokuGenerator.addNumber(testBoard, 1, 2, 3);
+		assertEquals(1, testBoard[1][2]);
+	}
+
+	@Test
+	public void testAddNextNumberToSecondRowWithTwoNumbers() {
+		int[][] testBoard = { { 1, 2, 3, 4 }, { 1, 0 } };
+		SudokuGenerator.addNumber(testBoard, 1, 1, 2);
+		assertEquals(2, testBoard[1][1]);
 	}
 }
