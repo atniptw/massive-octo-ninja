5e63fab32a3f3b348e57c35a17165b8c7335130f
George Mammarella <mammargs@rose-hulman.edu>
2013-04-12 04:42:56 -0400

    Revises tests. Adds more useful tests. Adds code to generate incomplete boards from Tom's complete boards. Currently only accounts for difficulty by overall number of givens -- not givens per row/column, etc.

--- Sudoku/src/Sudoku/BoardAdjuster.java
+++ Sudoku/src/Sudoku/BoardAdjuster.java
@@ -1,26 +1,86 @@
 package Sudoku;
 
+import java.util.Random;
+
 public class BoardAdjuster {
 
 	public enum Difficulty {
 		VERY_EASY, EASY, MEDIUM, DIFFICULT, EVIL;
 	}
 
+	public static final int VERY_EASY_MAX_GIVEN = 70;
+	public static final int VERY_EASY_MIN_GIVEN = 50;
+	public static final int EASY_MAX_GIVEN = 49;
+	public static final int EASY_MIN_GIVEN = 36;
+	public static final int MEDIUM_MAX_GIVEN = 35;
+	public static final int MEDIUM_MIN_GIVEN = 32;
+	public static final int DIFFICULT_MAX_GIVEN = 31;
+	public static final int DIFFICULT_MIN_GIVEN = 28;
+	public static final int EVIL_MAX_GIVEN = 27;
+	public static final int EVIL_MIN_GIVEN = 22;
+
 	public static int[][] adjustForDifficulty(int[][] originalBoard,
 			Difficulty diff) {
-		
-		int[][] newBoard = null;
+
+		int[][] newBoard = originalBoard;
+
+		int variance;
+		int givens = 0;
+		int toRemove;
+		int maxBoardCells = originalBoard.length * originalBoard.length;
+
+		Random gen = new Random();
+
+		switch (diff) {
+
+		case VERY_EASY:
+
+			variance = gen.nextInt((VERY_EASY_MAX_GIVEN - VERY_EASY_MIN_GIVEN));
+			givens = VERY_EASY_MIN_GIVEN + variance;
+			break;
+
+		case EASY:
+
+			variance = gen.nextInt((EASY_MAX_GIVEN - EASY_MIN_GIVEN));
+			givens = EASY_MIN_GIVEN + variance;
+			break;
+
+		case MEDIUM:
+
+			variance = gen.nextInt((MEDIUM_MAX_GIVEN - MEDIUM_MIN_GIVEN));
+			givens = MEDIUM_MIN_GIVEN + variance;
+			break;
+
+		case DIFFICULT:
+
+			variance = gen.nextInt((DIFFICULT_MAX_GIVEN - DIFFICULT_MIN_GIVEN));
+			givens = DIFFICULT_MIN_GIVEN + variance;
+			break;
+
+		case EVIL:
+
+			variance = gen.nextInt((EVIL_MAX_GIVEN - EVIL_MIN_GIVEN));
+			givens = EVIL_MIN_GIVEN + variance;
+			
+
+		}
+
+		toRemove = maxBoardCells - givens;
+
+		while (toRemove > 0) {
+			int i = gen.nextInt(originalBoard.length - 1);
+			int j = gen.nextInt(originalBoard.length - 1);
+
+			if (newBoard[i][j] != 0) {
+				newBoard[i][j] = 0;
+			} else {
+				continue;
+			}
+			toRemove--;
+		}
 
 		return newBoard;
 
-	}
-	
-	private int[][] removeNumbers(int[][] board){
-		
-		int[][] newBoard = null;
-
-		return newBoard;
-		
 	}
 
 }
--- Sudoku/src/SudokuTest/BoardAdjusterTest.java
+++ Sudoku/src/SudokuTest/BoardAdjusterTest.java
@@ -2,50 +2,177 @@ package SudokuTest;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import Sudoku.BoardAdjuster;
+import Sudoku.CellBlock;
 import Sudoku.SudokuGenerator;
-
-
+import Sudoku.SudokuStandardRegion;
 
 public class BoardAdjusterTest {
-	
-	private static int[][] testBoard;
+
+	private int[][] testBoard;
 
 	@Test
-	public void testVeryEasyHasEnoughGivens() {
+	public void testVeryEasyAdjustedBoardIsInvalid() {
+
+		int[][] adjustedBoard;
+
 		testBoard = SudokuGenerator.generateBoard(9);
-		BoardAdjuster.adjustForDifficulty(testBoard, BoardAdjuster.Difficulty.VERY_EASY);
-		assertTrue(testBoard.length >= 50);
+		adjustedBoard = BoardAdjuster.adjustForDifficulty(testBoard,
+				BoardAdjuster.Difficulty.VERY_EASY);
+
+		SudokuStandardRegion region;
+		for (int i = 0; i < 9; i++) {
+			ArrayList<CellBlock> row = new ArrayList<CellBlock>();
+			for (int j = 0; j < 9; j++) {
+				row.add(new CellBlock(adjustedBoard[i][j]));
+			}
+			region = new SudokuStandardRegion(row);
+			if (region.isValid() && i < 9) {
+				continue;
+			} else {
+				assertFalse(region.isValid());
+			}
+		}
 	}
-	
+
 	@Test
-	public void testEasyHasEnoughGivens() {
+	public void testEasyAdjustedBoardIsInvalid() {
+
+		int[][] adjustedBoard;
+
 		testBoard = SudokuGenerator.generateBoard(9);
-		BoardAdjuster.adjustForDifficulty(testBoard, BoardAdjuster.Difficulty.EASY);
-		assertTrue(testBoard.length >= 36 && testBoard.length <= 49);
+		adjustedBoard = BoardAdjuster.adjustForDifficulty(testBoard,
+				BoardAdjuster.Difficulty.EASY);
+
+		SudokuStandardRegion region;
+		for (int i = 0; i < 9; i++) {
+			ArrayList<CellBlock> row = new ArrayList<CellBlock>();
+			for (int j = 0; j < 9; j++) {
+				row.add(new CellBlock(adjustedBoard[i][j]));
+			}
+			region = new SudokuStandardRegion(row);
+			if (region.isValid() && i < 9) {
+				continue;
+			} else {
+				assertFalse(region.isValid());
+			}
+		}
 	}
-	
+
 	@Test
-	public void testMediumHasEnoughGivens() {
+	public void testMediumAdjustedBoardIsInvalid() {
+
+		int[][] adjustedBoard;
+
 		testBoard = SudokuGenerator.generateBoard(9);
-		BoardAdjuster.adjustForDifficulty(testBoard, BoardAdjuster.Difficulty.MEDIUM);
-		assertTrue(testBoard.length >= 32 && testBoard.length <= 35);
+		adjustedBoard = BoardAdjuster.adjustForDifficulty(testBoard,
+				BoardAdjuster.Difficulty.MEDIUM);
+
+		SudokuStandardRegion region;
+		for (int i = 0; i < 9; i++) {
+			ArrayList<CellBlock> row = new ArrayList<CellBlock>();
+			for (int j = 0; j < 9; j++) {
+				row.add(new CellBlock(adjustedBoard[i][j]));
+			}
+			region = new SudokuStandardRegion(row);
+			if (region.isValid() && i < 9) {
+				continue;
+			} else {
+				assertFalse(region.isValid());
+			}
+		}
 	}
-	
+
 	@Test
-	public void testDifficultHasEnoughGivens() {
+	public void testDifficultAdjustedBoardIsInvalid() {
+
+		int[][] adjustedBoard;
+
 		testBoard = SudokuGenerator.generateBoard(9);
-		BoardAdjuster.adjustForDifficulty(testBoard, BoardAdjuster.Difficulty.DIFFICULT);
-		assertTrue(testBoard.length >= 28 && testBoard.length <= 31);
+		adjustedBoard = BoardAdjuster.adjustForDifficulty(testBoard,
+				BoardAdjuster.Difficulty.DIFFICULT);
+
+		SudokuStandardRegion region;
+		for (int i = 0; i < 9; i++) {
+			ArrayList<CellBlock> row = new ArrayList<CellBlock>();
+			for (int j = 0; j < 9; j++) {
+				row.add(new CellBlock(adjustedBoard[i][j]));
+			}
+			region = new SudokuStandardRegion(row);
+			if (region.isValid() && i < 9) {
+				continue;
+			} else {
+				assertFalse(region.isValid());
+			}
+		}
 	}
-	
+
 	@Test
-	public void testEvilHasEnoughGivens() {
+	public void testEvilAdjustedBoardIsInvalid() {
+
+		int[][] adjustedBoard;
+
 		testBoard = SudokuGenerator.generateBoard(9);
-		BoardAdjuster.adjustForDifficulty(testBoard, BoardAdjuster.Difficulty.EVIL);
-		assertTrue(testBoard.length >= 22 && testBoard.length <= 27);
+		adjustedBoard = BoardAdjuster.adjustForDifficulty(testBoard,
+				BoardAdjuster.Difficulty.EVIL);
+
+		SudokuStandardRegion region;
+		for (int i = 0; i < 9; i++) {
+			ArrayList<CellBlock> row = new ArrayList<CellBlock>();
+			for (int j = 0; j < 9; j++) {
+				row.add(new CellBlock(adjustedBoard[i][j]));
+			}
+			region = new SudokuStandardRegion(row);
+			if (region.isValid() && i < 9) {
+				continue;
+			} else {
+				assertFalse(region.isValid());
+			}
+		}
 	}
+
+	// @Test
+	// public void testVeryEasyHasEnoughGivens() {
+	//
+	// BoardAdjuster.adjustForDifficulty(testBoard,
+	// BoardAdjuster.Difficulty.VERY_EASY);
+	// assertTrue(testBoard.length >= 50);
+	// }
+	//
+	// @Test
+	// public void testEasyHasEnoughGivens() {
+	//
+	// BoardAdjuster.adjustForDifficulty(testBoard,
+	// BoardAdjuster.Difficulty.EASY);
+	// assertTrue(testBoard.length >= 36 && testBoard.length <= 49);
+	// }
+	//
+	// @Test
+	// public void testMediumHasEnoughGivens() {
+	//
+	// BoardAdjuster.adjustForDifficulty(testBoard,
+	// BoardAdjuster.Difficulty.MEDIUM);
+	// assertTrue(testBoard.length >= 32 && testBoard.length <= 35);
+	// }
+	//
+	// @Test
+	// public void testDifficultHasEnoughGivens() {
+	//
+	// BoardAdjuster.adjustForDifficulty(testBoard,
+	// BoardAdjuster.Difficulty.DIFFICULT);
+	// assertTrue(testBoard.length >= 28 && testBoard.length <= 31);
+	// }
+	//
+	// @Test
+	// public void testEvilHasEnoughGivens() {
+	//
+	// BoardAdjuster.adjustForDifficulty(testBoard,
+	// BoardAdjuster.Difficulty.EVIL);
+	// assertTrue(testBoard.length >= 22 && testBoard.length <= 27);
+	// }
 
 }
