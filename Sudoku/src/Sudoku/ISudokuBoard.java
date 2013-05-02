bfd9092e7675be46ef2479b109c1934350a7469c
George Mammarella <mammargs@rose-hulman.edu>
2013-05-01 21:36:31 -0400

    Fixes broken stuff

--- Sudoku/src/Sudoku/ISudokuBoard.java
+++ Sudoku/src/Sudoku/ISudokuBoard.java
@@ -1,20 +1,19 @@
 package Sudoku;
 
-
-
 public interface ISudokuBoard {
-	
 
 	public int getAnswer(int row, int column);
-	
+
 	public void setAnswer(int row, int column, int answer);
-	
+
 	public CellBlock getCell(int row, int column);
-	
+
 	public boolean isValid();
-	
+
 	public void setConflictingCellsToInvalid();
-	
+
 	public int size();
+
+	public void populateBoard();
 
 }
--- Sudoku/src/Sudoku/Sudoku.java
+++ Sudoku/src/Sudoku/Sudoku.java
@@ -21,23 +21,19 @@ public class Sudoku {
 	 */
 
 	public static void main(String[] args) {
-		/*ArrayList<CellBlock> blocks = new ArrayList<CellBlock>();
-		int[][] board = SudokuGenerator.generateBoard(9);
-		for (int i = 0; i < 9; i++) {
-			for (int j = 0; j < 9; j++) {
-				CellBlock temp = new CellBlock();
-				temp.setAnswer(board[i][j]);
-				blocks.add(temp);
-			}
-		}*/
-		
-		StandardSudokuBoard gameBoard = new StandardSudokuBoard(9);
-		gameBoard.populateBoard();
+		/*
+		 * ArrayList<CellBlock> blocks = new ArrayList<CellBlock>(); int[][]
+		 * board = SudokuGenerator.generateBoard(9); for (int i = 0; i < 9; i++)
+		 * { for (int j = 0; j < 9; j++) { CellBlock temp = new CellBlock();
+		 * temp.setAnswer(board[i][j]); blocks.add(temp); } }
+		 */
 
-		gameBoard.setConflictingCellsToInvalid();
+		// StandardSudokuBoard gameBoard = new StandardSudokuBoard(9);
+		// gameBoard.populateBoard();
+		//
+		// gameBoard.setConflictingCellsToInvalid();
 
 		SudokuFrame frame = new SudokuFrame(args[0], args[1]);
-
 
 	}
 }
